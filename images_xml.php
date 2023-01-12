e3dd0d62f59cfc9cb711f822efc7947f9daddee2
chpa-odoo <chpa@odoo.com>
2023-01-12 13:44:13 +0100

    Initial commit

---
+++ images_xml.php
@@ -0,0 +1,58 @@
+<?php
+
+// Set the directory to scan
+//$directory = '/Users/odoo/Documents/GitHub/psbe-design-custom/website_haillot/static/src/img/content';
+$directory = $_POST["directory"];
+
+// Create a variable that matches $directory but removes everything before "website_"
+$relative_directory = strstr($directory, 'website_');
+
+// Set the filename for the output file
+$output_file = 'images.xml';
+
+// Initialize an empty array to store the filenames
+$filenames = array();
+
+// Open the directory
+if ($handle = opendir($directory)) {
+    // Loop through the directory
+    while (false !== ($file = readdir($handle))) {
+        // Check if the file is an image
+        if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $file)) {
+            // Add the filename to the array 
+            $filenames[] = $file;
+        }
+    }
+    closedir($handle);
+}
+
+// Start the XML output
+$output = '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
+$output .= '<odoo>'.PHP_EOL;
+
+// Loop through the filenames and add them to the XML output
+foreach ($filenames as $filename) {
+    $output .='<record id="' . pathinfo(basename($filename), PATHINFO_FILENAME) . '" model="ir.attachment">'.PHP_EOL;
+    $output .= "\t" . '<field name="name">' . pathinfo(basename($filename), PATHINFO_FILENAME) . '</field>'.PHP_EOL;
+    $output .= "\t" . '<field name="datas" type="base64" file="/' . $relative_directory . $filename . '"/>'.PHP_EOL; 
+    $output .= "\t" . '<field name="res_model">' . ir.ui.view . '</field>'.PHP_EOL; 
+    $output .= "\t" . '<field name="public" eval="True"/>'.PHP_EOL;
+    $output .='</record>'.PHP_EOL;
+} 
+
+// End the XML output
+$output .= '</odoo>';
+
+// Check if the output file already exists
+if (file_exists($output_file)) {
+    // Delete the file if it exists
+    unlink($output_file);
+}
+
+// Write the output to the file
+file_put_contents($output_file, $output);
+
+// Redirect
+header("Location: index.php");
+die();
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+
+<style>
+.code-editor {
+    font-family: monospace;
+    white-space: pre;
+}
+</style>
+
+<form action="images_xml.php" method="post">
+    Directory: <input type="text" name="directory">
+    <input type="submit" value="Submit">
+</form> 
+
+<pre class="code-editor">
+    <?php
+    $xml = simplexml_load_file("images.xml") or die("File empty or not found");
+    echo htmlentities($xml->asXML());
+    ?>
+</pre>
+
+
+
+
+
+</body>
+</html>
